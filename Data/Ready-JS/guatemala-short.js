6c663c67627875e375bb8e9e4f7c0a8c9cddf259
Jorge Naranjo <jonarbo@gmail.com>
2018-05-09 10:06:42 +0400

    index.html for github-pages added

---
+++ Data/Ready-JS/guatemala-short.js
@@ -0,0 +1,62 @@
+eqfeed_callback (
+        {"type" : "FeatureCollection" ,
+         "features":
+                 [
+                   { "type":"Feature" ,
+                     "properties": 
+                                    {
+					"R":0.333333333,
+					"RR":1
+                                    },
+                      "geometry": {
+                                        "type":"Point",
+					"coordinates":[-92.306,14.5544]
+                                  }
+                    },
+                   { "type":"Feature" ,
+                     "properties": 
+                                    {
+					"R":0.333333333,
+					"RR":1
+                                    },
+                      "geometry": {
+                                        "type":"Point",
+					"coordinates":[-92.306,14.5592]
+                                  }
+                    },
+                   { "type":"Feature" ,
+                     "properties": 
+                                    {
+					"R":0.333333333,
+					"RR":1
+                                    },
+                      "geometry": {
+                                        "type":"Point",
+					"coordinates":[-92.3012,14.5544]
+                                  }
+                    },
+                   { "type":"Feature" ,
+                     "properties": 
+                                    {
+					"R":0.333333333,
+					"RR":1
+                                    },
+                      "geometry": {
+                                        "type":"Point",
+					"coordinates":[-92.3012,14.5592]
+                                  }
+                    },
+                   { "type":"Feature" ,
+                     "properties": 
+                                    {
+					"R":0.333333333,
+					"RR":1
+                                    },
+                      "geometry": {
+                                        "type":"Point",
+					"coordinates":[-92.2964,14.5448]
+                                  }
+                    }
+                 ]
+        }
+);
