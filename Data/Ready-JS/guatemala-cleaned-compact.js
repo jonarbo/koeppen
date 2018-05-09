4d7141231a89ab1ac87c62412d4b50a1ca738c0b
Jorge Naranjo <jonarbo@gmail.com>
2018-05-09 12:15:31 +0400

    removed LFS support

--- Data/Ready-JS/guatemala-cleaned-compact.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7c56b67bdc8891e830d7618c179d495f1a01cc60c379a86fc7cf7b848112f721
-size 54038540
--- Data/Ready-JS/guatemala-short.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ee7f5a1b664b3cee85800617fc1ee307629379e8fe7b8428b090021ae4deefc4
-size 2007
